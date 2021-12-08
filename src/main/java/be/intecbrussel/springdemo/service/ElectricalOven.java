fc6ebe37811d2bf58311cceced67999307508766
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-08 21:28:08 +0100

    event handling

--- src/main/java/be/intecbrussel/springdemo/service/ElectricalOven.java
+++ src/main/java/be/intecbrussel/springdemo/service/ElectricalOven.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.springdemo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,9 @@ public class ElectricalOven implements Oven {
     public ElectricalOven() {
         System.out.println("Creating Electrical Oven");
     }
+
+    @Autowired
+    private MessageSource msgSource;
 
     @Override
     public void bakePizza() {
